1355ed86681754fe88ece02f6c9d547a0887e9da
Bri in The Sky <brianh6854@googlemail.com>
2016-05-17 16:58:24 +0100

    Works to localhost

--- test.cr
+++ test.cr
@@ -1,1 +1,11 @@
 require "./src/pcap"
+
+# First test lookup interface
+
+BUFSIZE = LibPcap::PCAP_ERRBUF_SIZE
+
+cap = Pcap.new
+v = cap.lookupdev("")
+errbuf = Pointer(UInt8).new(BUFSIZE)
+dev = cap.open_live("lo", BUFSIZE, 65000, 0, errbuf)
+puts dev
