1a9df4f9e0e933088bd08378c8ae69329d9f9c9f
Bri in The Sky <brianh6854@googlemail.com>
2016-05-19 17:59:14 +0100

    Added functions

--- src/pcap.cr
+++ src/pcap.cr
@@ -1,1 +1,36 @@
 require "./pcap/*"
+
+class Pcap
+  
+  #BpfUInt32 = LibPcap::BpfUInt32
+  #BpfProgram = LibPcap::BpfProgram
+  
+  def lookupdev(dev : String)
+    LibPcap.pcap_lookupdev(dev)
+  end
+  
+  def lookupnet(dev : String, netp, maskp, errbuf)
+    LibPcap.pcap_lookupnet(dev, netp, maskp, errbuf)
+  end
+
+  def create(pointer, iface)
+    LibPcap.pcap_create(pointer, iface)
+  end
+
+  def compile(handle, bpfprogram, str, optimize, netmask)
+    LibPcap.pcap_compile(handle, bpfprogram, str, optimize, netmask)
+  end
+  
+  def setfilter(handle, bpfprogram)
+    LibPcap.pcap_setfilter(handle, bpfprogram)
+  end
+  
+  def open_live(dev : String, bufsize, snaplen, promisc, timeout)
+    LibPcap.pcap_open_live(dev, bufsize, snaplen, promisc, timeout)
+  end
+  
+  def next(handle, header)
+    LibPcap.pcap_next(handle, header)
+  end
+  
+end
